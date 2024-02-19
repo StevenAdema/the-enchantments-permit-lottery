a1af68a8dbf0f7d56dc90e5ae73b821132dd30db
Steven Adema <stevenadema@gmail.com>
2024-02-18 22:41:38 -0500

    readme update with example

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ df = utils.read_txt_to_df(txt_path, csv_path, save_csv=True)
 # Get only July dates
 # df = df[(df['application_date'] >= '2023-05-15') & (df['application_date'] <= '2023-07-31')]
 # df = df[(df['application_date'] >= '2023-08-01') & (df['application_date'] <= '2023-10-31')]
-df = df[(df['application_date'] >= '2023-07-15') & (df['application_date'] <= '2023-07-21')]
+df = df[(df['application_date'] >= '2023-07-22') & (df['application_date'] <= '2023-07-28')]
 
 # Print heat map of lottery permit results
 utils.plot_heatmap(df)
